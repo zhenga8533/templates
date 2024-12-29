fdc44f86edcfcfc30056b3a52d636f6c752bebc6
Allen Zheng <zhenga8533@gmail.com>
2024-12-29 17:45:17 -0500

    implemented logging

--- python/src/main.py
+++ python/src/main.py
@@ -1,2 +1,11 @@
+from dotenv import load_dotenv
+from util.logger import Logger
+import logging
+import os
+
 if __name__ == "__main__":
-    print("Hello, World!")
+    load_dotenv()
+    LOG = os.getenv("LOG") == "True"
+
+    logger = Logger("main", "logs/main.log", LOG)
+    logger.log(logging.INFO, "Hello, world!")
