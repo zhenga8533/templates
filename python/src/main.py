783a734ab8f10546be07ce7c5ea9f4de57d7f914
Allen Zheng <zhenga8533@gmail.com>
2025-03-19 11:38:47 -0400

    Updated python template

--- python/src/main.py
+++ python/src/main.py
@@ -1,11 +1,26 @@
-from dotenv import load_dotenv
-from util.logger import Logger
-import logging
 import os
 
-if __name__ == "__main__":
+from dotenv import load_dotenv
+from util.logger import Logger
+
+
+def main(s: str) -> None:
+    """
+    Main function of the program.
+
+    :param s: A string to print.
+    :return: None
+    """
+
+    # Load environment variables
     load_dotenv()
     LOG = os.getenv("LOG") == "True"
 
+    # Initialize logger
     logger = Logger("main", "logs/main.log", LOG)
-    logger.log(logging.INFO, "Hello, world!")
+
+    logger.info(s)
+
+
+if __name__ == "__main__":
+    main("Hello, World!")
